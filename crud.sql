fb5cdd7e6283adde2963e116d594a787f3888739
netomilfont <netomilfont@hotmail.com>
2022-12-03 01:44:46 +0100

    aprendendo banco de dados

--- crud.sql
+++ crud.sql
@@ -4,31 +4,65 @@
 -- Criação
 
 -- 1)
-
-
+ INSERT INTO clientes
+ 	(nome, lealdade)
+ VALUES
+	('Georgia', 0);
 
 -- 2)
-
-
+INSERT INTO pedidos 
+	(status, cliente_id)
+VALUES 
+	('Recebido', 6);
 
 -- 3)
-
-
+INSERT INTO produtos_pedidos 
+	(pedido_id, produto_id)
+VALUES 
+	(6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8);
 
 -- Leitura
 
 -- 1)
-
-
+SELECT
+ 	cl.*,
+ 	ped.*,
+ 	prod.id,
+ 	prod.nome,
+ 	prod.tipo,
+ 	ROUND((prod.preço)::NUMERIC, 2) preço,
+ 	prod.pts_de_lealdade
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id
+ JOIN
+ 	clientes cl ON ped.id = cl.id
+ WHERE
+ 	cl.nome LIKE 'Georgia';
 
 -- Atualização
 
 -- 1)
-
+UPDATE 
+ 	clientes cl
+ SET
+	lealdade = 20
+ WHERE
+	cl.nome LIKE 'Georgia';
 
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM
+	clientes cl
+WHERE
+	cl.nome LIKE 'Marcelo';
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,70 @@
 -- Seleções de dados
 
 -- 1)
-
--- 2)
-
--- 3)
-
--- 4)
+SELECT
+ 	ped.*,
+ 	prod.id,
+ 	prod.nome,
+ 	prod.tipo,
+ 	ROUND((prod.preço)::NUMERIC, 2) preço,
+ 	prod.pts_de_lealdade 
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id;
+ 
+ -- 2)
+ SELECT
+ 	ped.id
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id
+ WHERE 
+ 	prod.nome LIKE 'Fritas';
+ 
+ -- 3)
+ SELECT
+ 	cl.nome 
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id
+ JOIN
+ 	clientes cl ON ped.id = cl.id
+ WHERE
+ 	prod.nome LIKE 'Fritas';
+ 
+ -- 4)
+SELECT
+ 	SUM(ROUND((prod.preço)::NUMERIC,  2))
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id
+ JOIN
+ 	clientes cl ON ped.id = cl.id
+ WHERE
+ 	cl.nome LIKE 'Laura';
 
 -- 5)
+ SELECT
+ 	prod.nome,
+ 	COUNT(prod.nome)
+ FROM
+ 	pedidos ped
+ JOIN
+ 	produtos_pedidos pp ON ped.id = pp.pedido_id
+ JOIN
+ 	produtos prod ON pp.produto_id = prod.id
+ GROUP BY
+ 	prod.nome HAVING COUNT(prod.nome) >= 0 ORDER BY prod.nome ASC;
+ 
